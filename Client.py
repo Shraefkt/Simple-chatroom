22803195eae43a388736828b4ba8fe9e0b038ddd
Shraefkt <74842465+Shraefkt@users.noreply.github.com>
2020-11-23 10:50:07 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -15,7 +15,7 @@ DISCONNECT_MSG = "!DISCONNECTED"
 ADDRESS = (SERVER,PORT)
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect(ADDRESS)
-recieve = True
+
 def on_closing():
         send_message(DISCONNECT_MSG)
 def display_receive():
@@ -37,15 +37,27 @@ def send_message(msg):
     client.send(message)
     print(f"[{CLIENT}]: {message.decode(FORMAT)}")
     display_receive()
-class tkpage:
+class tkpages:
+    mode = True
     def __init__(self):
-        self.GUI()
-
-    def GUI(self):
         self.root = tk.Tk()
         self.canvas = tk.Canvas(self.root, height=500, width=600)
         self.canvas.pack()
+        self.menu_main()
+        self.root.mainloop()
+        on_closing()
+    def menu_main(self):
+        self.menu = tk.Menu(self.root)
+        self.root.config(menu=self.menu)
 
+        self.homemenu = tk.Menu(self.menu, activebackground="blue", tearoff=0)
+        self.menu.add_cascade(label="Menu", menu=self.homemenu)
+        self.homemenu.add_command(label="Settings", command=lambda: self.settings())
+        self.homemenu.add_command(label="Chatroom", command=lambda: self.GUI())
+        #self.homemenu.add_command(label="Favourites", command=lambda: favourite_page())
+
+    def GUI(self):
+        self.page_setup()
         self.displayframe1 = tk.Frame(self.root, bg="black", bd=5)
         self.displayframe1.place(relx=0.5, rely=0.05, relwidth=0.9, relheight=0.65, anchor="n")
 
@@ -67,8 +79,6 @@ class tkpage:
         self.mylist = tk.Listbox(self.displayframe1, yscrollcommand=self.scrollbar.set)
         self.mylist.pack(side="left", fill="both",expand = "true")
         self.scrollbar.config(command=self.mylist.yview)
-        self.root.mainloop()
-        on_closing()
     def refresh(self):
         try:
             display_receive()
@@ -87,11 +97,34 @@ class tkpage:
         send_message(msg)
         self.entry.delete(0, "end")
 
-    def sure_quit(self):
-        msgb = messagebox.askyesno("Quit?")
-        if msgb == "Yes":
-            on_closing()
+    def settings(self):
+        self.page_setup()
+        self.settingsframe = tk.Frame(self.root,bg = "black",bd = 15)
+        self.settingsframe.place(relx = 0.5 ,rely = 0.05,relwidth = 0.9 , relheight = 0.9,anchor = "n")
+
+        self.nickname_label = tk.Label(self.settingsframe,text = "Nickname:",font =("tw cen mt condensed", 25) )
+        self.nickname_label.place(relx = 0.02,rely = 0.1, relwidth = 0.25,relheight = 0.1)
+        self.nickname_entry = tk.Entry(self.settingsframe,font=("tw cen mt condensed", 25))
+        self.nickname_entry.place(relx = 0.3, rely =0.1, relwidth = 0.69,relheight = 0.1)
+
+        self.nickname_button = tk.Button(self.settingsframe,relief = "solid",text = "Change",font=("tw cen mt condensed", 25),command = lambda : send_message("N/"+self.nickname_entry.get()))
+        self.nickname_button.place(relx = 0.02,rely = 0.25,relwidth = 0.25,relheight = 0.15)
+
+        self.darkmode_button = tk.Button(self.settingsframe,relief = "solid",text = "Dark/Light Mode",font=("tw cen mt condensed",25))
+        self.darkmode_button.place(rely = 0.8)
+
+    def colour_mode(self):
+        if mode == True:
+            print("mode : dark")
         else:
-            pass
-page = tkpage()
+            print("mode :light")
+    def changepage(self):
+        for widget in self.root.winfo_children():
+            widget.destroy()
+
+    def page_setup(self):
+        self.changepage()
+        self.menu_main()
+
+page = tkpages()
 
